16175e4606ebf21ccf5bf2335b8e243f010f50b4
uniquesreedhar <97074135+uniquesreedhar@users.noreply.github.com>
2024-04-25 09:17:17 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,6 @@
 s3path=$(aws ssm get-parameters --names s3path --with-decryption --query Parameters[0].Value --output text --region ap-south-1)
 
 aws s3 cp $s3path  .
-unzip  5.zip 
+unzip -j 5.zip -d 5
+cd 5
 
